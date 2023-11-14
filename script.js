0ae960f27f775f7fdbfe800d6061adbecde4d9ff
Ruslan270111 <ruslan.baishev94@gmail.com>
2023-11-14 22:28:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showRegistrationForm() {
+    var mainBlock = document.querySelector('main');
+    var registrationForm = document.getElementById('registrationForm');
+
+    mainBlock.style.display = 'none';
+    registrationForm.style.display = 'block';
+}
